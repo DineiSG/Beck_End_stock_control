75c440ece8a4db53602acc858c04dd3069a6ff39
Dinei <waldinei.wsg@gmail.com>
2024-05-10 11:33:35 -0300

    Adicionado o plugin Lombok para a chamada Getters e Setters

--- src/main/java/com/autoshopping/stock_control/api/Veiculos/Veiculos.java
+++ src/main/java/com/autoshopping/stock_control/api/Veiculos/Veiculos.java
@@ -3,10 +3,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Optional;
 
-
+@Data
 @Entity
 @Table(name = "veiculos")
 public class Veiculos {
@@ -22,87 +25,8 @@ public class Veiculos {
     private String modelo;
     private String marca;
 
-    public Veiculos(){
-    }
-    public Veiculos(Integer id, String tag, String loja, String placa, String ano_fabricacao, String cor, String modelo, String marca){
-        this.id=id;
-        this.tag=tag;
-        this.loja=loja;
-        this.placa=placa;
-        this.ano_fabricacao=ano_fabricacao;
-        this.cor=cor;
-        this.modelo=modelo;
-        this.marca=marca;
-    }
 
-    public static Optional<Object> findByPlaca(String placa) {
 
-        return Optional.empty();
-    }
-
-    public Integer getId(){
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public String getLoja() {
-        return loja;
-    }
-
-    public void setLoja(String loja) {
-        this.loja = loja;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-
-    public String getAno_fabricacao() {
-        return ano_fabricacao;
-    }
-
-    public void setAno_fabricacao(String ano_fabricacao) {
-        this.ano_fabricacao = ano_fabricacao;
-    }
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
 
 
     public void save(Veiculos veiculo) {
